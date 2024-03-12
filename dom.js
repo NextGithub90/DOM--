4dd77f3e66aabb21282ee55cfad915bf344e9b7d
NextGithub90 <akunbeta958@gmail.com>
2024-03-12 08:01:18 +0700

    fastt

--- dom.js
+++ dom.js
@@ -1,12 +1,12 @@
 // DOM Selection
 // document.getElementsById() -> element
-const judul = document.getElementById("judul");
-judul.style.color = "blue";
-judul.innerHTML = "<marquee>Hello world</marquee>";
+// const judul = document.getElementById("judul");
+// judul.style.color = "blue";
+// judul.innerHTML = "<marquee>Hello world</marquee>";
 
 //document.getElementsByTagName() ->
 
-let p = document.getElementsByTagName("p");
+/* let p = document.getElementsByTagName("p");
 p[0].style.color = "blue";
 p[1].style.backgroundColor = "#ffebcd";
 
@@ -14,7 +14,19 @@ p[1].style.backgroundColor = "#ffebcd";
 for (let i = 0; i < p.length; i++) {
   p[i].style.border = "2px solid black";
 }
+ */
 
 // document.getElementsByClassName() -> htmlColeetion
-let cn = document.getElementsByClassName("halo");
-cn[0].style.backgroundColor = "grey";
+// let cn = document.getElementsByClassName("halo");
+// cn[0].style.backgroundColor = "grey";
+
+// query selector -> 1element
+// const li2 = document.querySelector("p");
+// li2.style.color = "red";
+// li2.style.backgroundColor = "black";
+
+// query selector all -> 1element
+const p = document.querySelectorAll("p");
+for (let i = 0; i < p.length; i++) {
+  p[i].innerHTML = "halo";
+}
---
+++ manipulationDom.js
@@ -0,0 +1,18 @@
+// setatribut
+// getatribut
+// removeatribut
+/* let judul = document.getElementById("judul");
+judul.setAttribute("class", "title");
+const classf = document.getElementsByClassName("title");
+classf[0].innerHTML = "<marquee>haloooo</marquee>";
+const a = document.querySelector("a");
+
+console.log(a.getAttribute("href")); */
+
+// classlist
+// classlist.add
+//  classlist.remove
+// classlist.contains // mengecek punya nggak class tertentu
+// classlist.toggle // akan menmbahkan class bila tk ada clas tetapi bila sudah memiliki class yg sama toggle akan menghapus class tersebut
+// classlist.item // mengetahui urutan n ama class tertentu
+// classlist.replace //mengganti class yg ada menggunakan classyg baru
